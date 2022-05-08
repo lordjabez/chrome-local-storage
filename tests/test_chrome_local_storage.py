5296efa4819a72ceb627429b48328ce48924e2f7
Judson Neer <judson.neer@gmail.com>
2022-05-08 07:15:48 -0700

    Validate version via pyproject.toml file.

--- tests/test_chrome_local_storage.py
+++ tests/test_chrome_local_storage.py
@@ -1,5 +1,12 @@
+import toml
+
 import chrome_local_storage
 
 
+with open('pyproject.toml') as project_file:
+    project = toml.load(project_file)
+
+
 def test_version():
-    assert chrome_local_storage.__version__ == '0.5.0'
+    version = project['tool']['poetry']['version']
+    assert chrome_local_storage.__version__ == version
